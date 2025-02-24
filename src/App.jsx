8069f460e23c4d6ef7f2182484ac5ff6865debb4
Juan Valencia <valenciacamilo5926@gmail.com>
2025-02-24 17:17:27 -0500

    Se creó el boton para agregar tareas, el contenedor para las notas y el modal que contene el formulario de las notas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [notas, setNotas] = useState([]); // Para almacenar las notas que se vayan generando
+
+  const mostrarModal = () => {
+    document.querySelector('#contenedorformularioParaNotas').style.display = 'flex';
+  }
+
+  const cerrarModal = () => {
+    document.querySelector('#contenedorformularioParaNotas').style.display = 'none';
+  }
 
   return (
     <>
-      
+      <div id="contenedorformularioParaNotas">
+        <form id="formularioParaNotas">
+          <div id="cerrar" onClick={cerrarModal}>X</div>
+          <h1> Agregar Nota:</h1>
+          <input type="text" placeholder="Titulo"/>
+          <textarea type="text" placeholder="Descripcion"/>
+        </form>
+      </div>
+
+      <div id="contenedor">
+        <button onClick={mostrarModal} id="agregarNota" type="submit">
+          +
+        </button>
+
+        <div id="contenedorDeNotas">
+          
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
