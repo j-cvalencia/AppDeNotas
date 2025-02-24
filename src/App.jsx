fdb63b43cef0a0810cb81a808cb9d6aa05ee0561
Juan Valencia <valenciacamilo5926@gmail.com>
2025-02-24 18:27:15 -0500

    Se configuró el formulario para crear las notas, se hicieron las notas junto con su diseño, falta crear la funcion para eliminar nota

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,56 @@ import "./App.css";
 function App() {
   const [notas, setNotas] = useState([]); // Para almacenar las notas que se vayan generando
 
+  const [inputTitulo, setInputTitulo] = useState("");
+  const [inputDescripcion, setInputDescripcion] = useState("");
+
   const mostrarModal = () => {
-    document.querySelector('#contenedorformularioParaNotas').style.display = 'flex';
-  }
+    document.querySelector("#contenedorformularioParaNotas").style.display =
+      "flex";
+  };
 
   const cerrarModal = () => {
-    document.querySelector('#contenedorformularioParaNotas').style.display = 'none';
-  }
+    document.querySelector("#contenedorformularioParaNotas").style.display =
+      "none";
+  };
+
+  const agregarTarea = (e) => {
+    e.preventDefault();
+    const nuevaNota = {
+      titulo: inputTitulo,
+      descripcion: inputDescripcion,
+    }
+    setNotas([...notas, nuevaNota]);
+
+    setInputTitulo('');
+    setInputDescripcion('');
+    
+    cerrarModal();
+  };
 
   return (
     <>
       <div id="contenedorformularioParaNotas">
-        <form id="formularioParaNotas">
-          <div id="cerrar" onClick={cerrarModal}>X</div>
+        <form onSubmit={agregarTarea} id="formularioParaNotas">
+          <div id="cerrar" onClick={cerrarModal}>
+            X
+          </div>
           <h1> Agregar Nota:</h1>
-          <input type="text" placeholder="Titulo"/>
-          <textarea type="text" placeholder="Descripcion"/>
+          <input 
+            value={inputTitulo} 
+            onChange={(e) => setInputTitulo(e.target.value)}
+            type="text" 
+            placeholder="Titulo" 
+          />
+          <textarea
+            value={inputDescripcion}
+            type="text"
+            onChange={(e) => setInputDescripcion(e.target.value)}
+            placeholder="Descripcion"
+          />
+          <button id="enviar" type="submit">
+            Enviar
+          </button>
         </form>
       </div>
 
@@ -29,7 +63,16 @@ function App() {
         </button>
 
         <div id="contenedorDeNotas">
-          
+          {notas.map((nota,index)=>{
+            return (
+              <div key={index} className="nota">
+                <button className="borrarNota"><i class="fa-solid fa-trash"></i></button>
+                <h2>{nota.titulo}</h2>
+                <hr />
+                <p>{nota.descripcion}</p>
+              </div>
+            )
+          })}
         </div>
       </div>
     </>
